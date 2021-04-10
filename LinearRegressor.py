589cbce0d7d64009358df0f797d7a11eb338a8a2
SK HAPIJUL HOSSEN <skhapijulhossen@gmail.com>
2021-04-10 09:46:01 +0530

    KFold Cross Validation Added

--- LinearRegressor.py
+++ LinearRegressor.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 
 # Loading Fuel Consumption Data
-data = pd.read_csv(r"C:\PYTHON\Lets_Solve\DataStructures\FuelConsumption.csv")
+data = pd.read_csv(r"C:\PYTHON\LinearRegression\FuelConsumption.csv")
 print(data.head(10))
-
-
+print(data.info())
+split = int(data.shape[0]*0.8)
 # Spliting Data into Train Test Set
-x_train = data[["ENGINESIZE"]][:int(data.shape[0]*0.8)].values
-y_train = data[["CO2EMISSIONS"]][:int(data.shape[0]*0.8)].values
-x_test = data[["ENGINESIZE"]][int(data.shape[0]*0.8):].values
-y_test = data[["ENGINESIZE"]][int(data.shape[0]*0.8):].values
+x_train = data[["ENGINESIZE"]][:split].values
+y_train = data[["CO2EMISSIONS"]][:split].values
+x_test = data[["ENGINESIZE"]][split:].values
+y_test = data[["CO2EMISSIONS"]][split:].values
 
 
 # Regression Model From Scratch
@@ -21,18 +21,35 @@ class LinearRegressor:
         self.intercept = 0
         self.coefficient = 0
         print("Linear Regressor Initialized!")
-    
 
     def fit(self, x, y):
         xbar = np.mean(x)
         ybar = np.mean(y)
-        self.coefficient = int(np.sum(((x-xbar) * (y- ybar)))/np.sum(((x-xbar)**2)))
+        self.coefficient = int(
+            np.sum(((x-xbar) * (y - ybar)))/np.sum(((x-xbar)**2)))
         self.intercept = int(ybar - (xbar * self.coefficient))
- 
 
     def predict(self, x):
-        yhat = np.array(self.intercept + (x * self.coefficient), dtype=np.int64)
+        yhat = np.array(self.intercept +
+                        (x * self.coefficient), dtype=np.int64)
         return yhat
+
+
+# Model Evaluators
+def mean_absolute_error(actual_y, yhat):
+    mae = (1/len(yhat)) * np.sum(np.abs(actual_y - yhat))
+    return mae
+
+
+def root_mean_squared_error(actual_y, yhat):
+    mae = (1/len(yhat)) * np.sum(np.square(np.abs(actual_y - yhat)))
+    return np.sqrt(mae)
+
+
+def r2_score(actual_y, yhat):
+    r2 = 1 - (np.sum(np.square(actual_y - yhat)) /
+              np.sum(np.square(actual_y - np.mean(actual_y))))
+    return r2
 
 
 # Initializing Model and Training MOdel
@@ -40,11 +57,44 @@ model = LinearRegressor()
 model.fit(x_train, y_train)
 
 
-# Predicting Model
-y_pred = model.predict(x_test)
+# K-Fold Cross Validation
+MAe = [] 
+MSe = []
+R2 =[]
+step = int(data.shape[0] * 0.2)
+low, up = 0,step
+for test in range(5):
+    if low == 0:
+        nx_train = data[["ENGINESIZE"]][up:].values
+        ny_train = data[["CO2EMISSIONS"]][up:].values
+    elif up == data.shape[0]:
+        nx_train = data[["ENGINESIZE"]][:low].values
+        ny_train = data[["CO2EMISSIONS"]][:low].values
+    else:
+        train_indexe1 = list(range(0,low))
+        train_indexe2 = list(range(up,data.shape[0]-1))
+        train_indexes = train_indexe1+train_indexe2
+
+    nx_test = data[["ENGINESIZE"]].iloc[list(range(low,up))].values
+    ny_test = data[["CO2EMISSIONS"]].iloc[list(range(low,up))].values
+
+    model = LinearRegressor()
+    model.fit(nx_train, ny_train)
+    ny_pred = model.predict(nx_test)
+
+    mae = mean_absolute_error(ny_test, ny_pred)
+    mse = root_mean_squared_error(ny_test, ny_pred)
+    r2 = r2_score(ny_test, ny_pred)
+    MAe.append(mae); MSe.append(mse); R2.append(r2)
+    print(">>>>>>>\n MAE: ", mae)
+    print("MSE: ",mse )
+    print("(Higher R2 Value Means Better Fit) R2: ", r2)
+    low , up = up, up+step
+
+    # Plotting Model
+    plt.scatter(nx_test, ny_test)
+    plt.plot(nx_test, ny_pred)
+    plt.show()
 
 
-# Plotting Model
-plt.scatter(data[["ENGINESIZE"]][int(data.shape[0]*0.8):].values, data[["CO2EMISSIONS"]][int(data.shape[0]*0.8):].values)
-plt.plot(data[["ENGINESIZE"]][int(data.shape[0]*0.8):].values, y_pred)
-plt.show()
+print(f">>>>>>>>>>>>>>>\n Average MAE {sum(MAe)/len(MAe)} \n Averagr MSE {sum(MSe)/len(MSe)} \n Average R2 {sum(R2)/len(R2)} ")
